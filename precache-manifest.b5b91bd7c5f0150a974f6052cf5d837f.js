f20147211d4ea126f391f717f11149fb8d554c98
sankshitpandoh <sankshit02pandoh.sp@gmail.com>
2020-07-10 00:57:36 +0530

    Updates

---
+++ precache-manifest.b5b91bd7c5f0150a974f6052cf5d837f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db51d377a69b2394502cf58ebbec008e",
+    "url": "/fluffy-image-search/index.html"
+  },
+  {
+    "revision": "a2bab26e2ddaa9dc4078",
+    "url": "/fluffy-image-search/static/css/main.86a51ff8.chunk.css"
+  },
+  {
+    "revision": "f57db4a5986de1813c83",
+    "url": "/fluffy-image-search/static/js/2.b36a7a99.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fluffy-image-search/static/js/2.b36a7a99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2bab26e2ddaa9dc4078",
+    "url": "/fluffy-image-search/static/js/main.5233e41a.chunk.js"
+  },
+  {
+    "revision": "304c6993ba2dbeda9869",
+    "url": "/fluffy-image-search/static/js/runtime-main.fd799dd6.js"
+  }
+]);
